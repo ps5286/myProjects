2eaef40581688178be4a32fad90f44143cc0176d
ps5286 <36456213+ps5286@users.noreply.github.com>
2018-05-11 08:04:19 -0500

    initial upload
    CSS stylesheet, Javascript app, and index HTML for Matching Game.

---
+++ app.js
@@ -0,0 +1,195 @@
+/*
+ * Create a list that holds all of your cards
+ */
+var playingDeck = [
+  "fa-diamond",
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-bolt",
+  "fa-cube",
+  "fa-cube",
+  "fa-bicycle",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-bomb",
+  "fa-leaf",
+  "fa-leaf",
+];
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+var beginGame = false;
+var openCard = [];
+var matches = 0;
+var attempts = 0;
+var moves = 0;
+var starRating = 0;
+var elapsedTime = 0;
+var elapsed = 0;
+var startGame = false;
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+function createCard() {
+  var gameList = shuffle(playingDeck);
+  gameList.forEach(function(card) {
+    $(".deck").append('<li class="card"><i class="fa ' + card + '"></i></li>');
+    });
+}
+
+
+
+function revealCard() {
+  $(".card").on("click", function() {
+    startGame = true;
+  if ($(this).hasClass("open show")) { return;}
+    $(this).toggleClass("open show");
+    openCard.push($(this));
+    setTimeout(checkMatch, 1000);
+});}
+
+function checkMatch() {
+  if (openCard.length === 2) {
+    if (openCard[0].children().attr("class") === openCard[1].children().attr("class")) {
+       openCard[0][0].classList.add("bounceIn", "match");
+       openCard[1][0].classList.add("bounceIn", "match");
+       $(openCard[0]).off("click");
+       $(openCard[1]).off("click");
+       matches += 1;
+       attempts++;
+       setTimeout(removeOpenCards, 500);
+       matchComplete();
+     }  else {
+          openCard[0][0].classList.add("shake");
+          openCard[1][0].classList.add("shake");
+          attempts++;
+          setTimeout(removeClasses, 500);
+          removeOpenCards();
+       }
+     }
+    updateMoves();
+    difficultyRating();
+  return;
+}
+
+function updateMoves() {
+  document.getElementById("attemptsText").innerHTML = attempts;
+}
+
+function matchComplete() {
+  if (matches == 8) {
+    elapsedTime = elapsed;
+    startGame = false;
+    alert("You've won in " + elapsed + " seconds" + " and in " + attempts + " attempts!");
+  }
+}
+
+function playAgain() {
+  $("restart").on("click", function() {
+       window.location.reload(true);
+});
+   clearInterval(timer);
+ }
+
+// Reset openCard.length to 0
+function removeOpenCards() {
+  openCard = [];
+}
+
+// Remove all classes except "match"
+function removeClasses() {
+  $(".card").removeClass("show open flipInY bounceIn shake wrong");
+  removeOpenCards();
+}
+
+// Disable clicks
+function disableClick() {
+ openCard.forEach(function (card) {
+   card.off("click");
+  });
+}
+
+
+
+function difficultyRating() {
+  if (attempts > 0 && attempts < 16) {
+     starRating = starRating;
+  } else if (attempts >= 16 && attempts <= 20) {
+     $(".stars").removeClass("fa fa-star");
+     starRating = "2";
+   } else if (attempts >= 21 && attempts <= 25) {
+   } else if (attempts > 20) {
+     $("#stars").removeClass("fa fa-star");
+     starRating = "1";
+   } else if (attempts > 25) {
+     $("#stars").removeClass("fa fa-star");
+     starRating = "0";
+    }
+}
+
+
+while(startGame == true) {
+  var start = new Date().getTime(),
+      time = 0,
+      elapsed = '0.0';
+
+  function instance()
+  {
+      time += 100;
+
+      elapsed = Math.floor(time / 100) / 10;
+      if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
+
+      document.title = elapsed;
+      document.getElementById("elapsedText").innerHTML = elapsed;
+      var diff = (new Date().getTime() - start) - time;
+      window.setTimeout(instance, (100 - diff));
+  }
+
+  window.setTimeout(instance, 100);
+}
+
+
+
+shuffle(playingDeck);
+createCard();
+revealCard();
+removeOpenCards();
+difficultyRating();
+matchComplete();
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
