c7bdadc16f424d66209b31a5d302d47d5a2367b7
ps5286 <36456213+ps5286@users.noreply.github.com>
2018-05-19 09:39:18 -0500

    Add files via upload
    Corrected timer variable in Modal popup to display elapsed time in modal popup
    Adjust timing in game
    fix bounceIn animation for a matched card

--- app.js
+++ app.js
@@ -21,20 +21,16 @@ var playingDeck = [
 ];
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+Global Variable list.
  */
 var openCard = [];
 var matches = 0;
 var attempts = 0;
-var starRating = 0;
 var elapsedTime = 0;
 var elapsed = 0;
 var starRating = 3;
-
-
+var timer = "";
+var elapsedText = "";
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -51,7 +47,7 @@ function shuffle(array) {
     return array;
 }
 
-
+/*function that creates the card in HTML and shuffles them for the game.*/
 function createCard() {
   var gameList = shuffle(playingDeck);
   gameList.forEach(function(card) {
@@ -60,15 +56,17 @@ function createCard() {
 }
 
 
-
+/*function flips the card over when player selects it*/
 function revealCard() {
   $(".card").on("click", function() {
   if ($(this).hasClass("open show")) { return;}
     $(this).toggleClass("open show");
     openCard.push($(this));
-    setTimeout(checkMatch, 500);
+    setTimeout(checkMatch, 400);
 });}
 
+
+/*function that checks whether the cards selected by the player match.*/
 function checkMatch() {
   if (openCard.length === 2) {
     if (openCard[0].children().attr("class") === openCard[1].children().attr("class")) {
@@ -78,15 +76,17 @@ function checkMatch() {
        $(openCard[1]).off("click");
        matches += 1;
        attempts++;
-       setTimeout(removeOpenCards, 500);
+       setTimeout(removeOpenCards, 400);
        if(matches === 8) {
+         window.clearInterval(timer);
          matchComplete();
+
        }
      }  else {
           openCard[0][0].classList.add("shake");
           openCard[1][0].classList.add("shake");
           attempts++;
-          setTimeout(removeClasses, 500);
+          setTimeout(removeClasses, 800);
           removeOpenCards();
        }
      }
@@ -94,10 +94,9 @@ function checkMatch() {
   return;
 }
 
+/*function updates the moves and the star rating on the main game page as the game progresses.*/
 function updateMoves() {
   document.getElementById("attemptsText").innerHTML = attempts;
-  /*$("#attempts").text(attempts.toString());*/
-
   if (attempts > 0 && attempts < 16) {
     starRating = starRating;
   } else if (attempts >= 16 && attempts <= 20) {
@@ -109,47 +108,40 @@ function updateMoves() {
   }
 }
 
-
+/*function determines if all matches have been completed.  Also, creates variables that display on the modal popup.*/
 function matchComplete() {
   elapsedTime = elapsed;
-  /*alert("You've won in " + elapsedTime + " seconds" + " and in " + attempts + " attempts!");
-  window.clearInterval(timer);*/
-  var modal = document.getElementById('win-popup');
+  console.log(elapsedText);
+  var modal = document.getElementById("win-popup");
   var span = document.getElementsByClassName("close")[0];
-
-    $("#total-attempts").text(attempts);
-    $("#total-stars").text(starRating);
-
-    modal.style.display = "block";
-
+  $("#totalAttempts").text(attempts);
+  $("#totalStars").text(starRating);
+  $("#elapsedTime").text(elapsed);
+  modal.style.display = "block";
   // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-   $("#play-again-btn").on("click", function() {
-       location.reload(true)
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+   $("#playAgainBtn").on("click", function() {
+       window.location.reload(true)
    });
-
    clearInterval(timer);
-
-
 }
 
-
-function playAgain() {
+/*This function creates a restart button on the main game page so a player can start over if desired.*/
+function reStart() {
   $("restart").on("click", function() {
   window.location.reload(true);
 });
    window.clearInterval(timer);
  }
 
-// Reset openCard.length to 0
+// Removes and cards that are in openCard whether by reStart or PlayAgain.*/
 function removeOpenCards() {
   openCard = [];
 }
 
-// Remove all classes except "match"
+// Removes classes from card after a match except for the class match.*/
 function removeClasses() {
   $(".card").removeClass("show open bounceIn shake wrong");
   removeOpenCards();
@@ -163,7 +155,7 @@ function disableClick() {
 }
 
 
-
+/* Timer code from https://www.sitepoint.com/creating-accurate-timers-in-javascript/ */
 var time = 0,
     elapsed = '0.0';
 
@@ -184,6 +176,7 @@ timer = window.setInterval(function()
 
 
 
+
 shuffle(playingDeck);
 createCard();
 revealCard();
@@ -191,8 +184,7 @@ revealCard();
 
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
+/* * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
